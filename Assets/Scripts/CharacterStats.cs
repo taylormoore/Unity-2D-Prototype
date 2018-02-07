66b056efc4861e9c9cdad63c5f1454987d2af994
Taylor Moore <taylorjmoore92@gmail.com>
2018-02-06 22:36:55 -0600

    Beginnings of player/enemy lifecycle.

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterStats : MonoBehaviour {
+
+    public int health = 10;
+    private int maxHealth = 10;
+    public Image healthbar;
+
+    public void ApplyDamage(int amount) {
+        health -= amount;
+        healthbar.fillAmount = (float)health / maxHealth;
+
+        if (health <= 0) {
+            Death();
+        }
+    }
+
+    public void Death() {
+        Destroy(gameObject);
+    }
+}
