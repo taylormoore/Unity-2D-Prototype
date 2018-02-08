5b1bcb7634c22d9444f46f4e46a27123931701f1
Taylor Moore <taylorjmoore92@gmail.com>
2018-02-07 21:45:39 -0600

    Added basic main menu.

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{ 
+
+	void Update ()
+    {
+		if ( Input.GetButtonDown( "Submit" ) )
+        {
+            SceneManager.LoadScene("Main");
+        }
+	}
+}
