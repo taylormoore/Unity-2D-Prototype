264af7198c2995f692ec662684eab9fe5f8c54e8
Zack <zacktobar13@gmail.com>
2018-02-11 16:46:30 -0800

    Simplified angle between vectors. Also arm sprite now flips when facing backwards

--- Assets/Scripts/Player/RotateArm.cs
+++ Assets/Scripts/Player/RotateArm.cs
@@ -3,28 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RotateArm : MonoBehaviour
-{ 
+{
+
+    public SpriteRenderer spriteRenderer;
 
     void Update()
     {
-        //Mouse Position in the world. It's important to give it some distance from the camera. 
-        //If the screen point is calculated right from the exact position of the camera, then it will
-        //just return the exact same position as the camera, which is no good.
-        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 10f);
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+        
+        float angle = Utility.RotationAmount(transform.position, mouseWorldPosition);
 
-        //Angle between mouse and this object
-        float angle = AngleBetweenPoints(transform.position, mouseWorldPosition);
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
 
-        //Ta daa
-        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        spriteRenderer.flipY = angle > 90 || angle < -90;
     }
-
-    float AngleBetweenPoints(Vector2 a, Vector2 b)
-    {
-        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
-    }
-
-
-
-
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,23 +6,25 @@ public class Projectile : MonoBehaviour
     Vector2 target;
     Vector2 startPosition;
     public float movementSpeed;
-    Vector2 direction;
+    Vector2 movementDirection;
     public int projectileDamage;
 
     void Start()
     {
-        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition );
         startPosition = transform.position;
-        direction = (target - startPosition).normalized;
-        float dotValue = Vector2.Dot ( direction, Vector2.down );
-        int rotateDirection = dotValue > 0 ? -1 : 1;
-        float rotateAmount = Mathf.Acos ( Vector2.Dot ( Vector2.right, direction ) ) * 180 / Mathf.PI;
-        transform.Rotate ( 0, 0, rotateAmount * rotateDirection ); 
+
+        // Rotate sprite
+        float rotateAmount = Utility.RotationAmount ( startPosition, target );
+        transform.Rotate ( 0, 0, rotateAmount );
+        
+        // Get direction sprite should move
+        movementDirection = ( target - startPosition ).normalized;
     }
 
     void Update ()
     {
-        transform.Translate(direction * Time.deltaTime * movementSpeed, Space.World);
+        transform.Translate( movementDirection * Time.deltaTime * movementSpeed, Space.World);
     }
 
     private void OnTriggerEnter2D ( Collider2D collision )
