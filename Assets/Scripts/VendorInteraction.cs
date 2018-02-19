ddd2f830afc9dad331445737973d0f52f944fa96
Taylor Moore <tjosephmoore@gmail.com>
2018-02-18 18:05:14 -0600

    Began vendor interaction

---
+++ Assets/Scripts/VendorInteraction.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class VendorInteraction : MonoBehaviour
+{
+
+    public float interactRange;
+
+    public GameObject vendorWindow;
+    public bool canInteract;
+    bool interactPressed;
+
+    private void Update()
+    {
+        if ( Input.GetButtonDown("Interact") )
+        {
+
+        }
+    }
+
+    public void OnTriggerStay2D( Collider2D collision )
+    {
+        if (  && collision.gameObject.tag == "Player" )
+        {
+           
+        }
+        Debug.Log("Swag");
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log(" Not Swag");
+    }
+}
