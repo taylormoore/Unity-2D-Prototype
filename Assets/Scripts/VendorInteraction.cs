70a59a7c4ab1b63e448f5c71ed95a3113dd83a8a
Taylor Moore <tjosephmoore@gmail.com>
2018-02-18 18:20:57 -0600

    Vendor window toggling properly

--- Assets/Scripts/VendorInteraction.cs
+++ Assets/Scripts/VendorInteraction.cs
@@ -6,28 +6,36 @@ public class VendorInteraction : MonoBehaviour
     public float interactRange;
 
     public GameObject vendorWindow;
-    public bool canInteract;
-    bool interactPressed;
+    bool shouldListen;
+    bool showingWindow;
 
     private void Update()
     {
-        if ( Input.GetButtonDown("Interact") )
+        if ( Input.GetButtonDown("Interact") && shouldListen)
         {
-
+            ToggleVendorWindow(true);
         }
     }
 
-    public void OnTriggerStay2D( Collider2D collision )
+    public void OnTriggerEnter2D( Collider2D collision )
     {
-        if (  && collision.gameObject.tag == "Player" )
+        if ( collision.gameObject.tag == "Player" )
         {
-           
+            shouldListen = true;
         }
-        Debug.Log("Swag");
     }
 
-    private void FixedUpdate()
+    public void OnTriggerExit2D(Collider2D collision)
     {
-        Debug.Log(" Not Swag");
+        if (collision.gameObject.tag == "Player")
+        {
+            shouldListen = false;
+            ToggleVendorWindow(false);
+        }
+    }
+
+    public void ToggleVendorWindow(bool toggle)
+    {
+        vendorWindow.SetActive(toggle);
     }
 }
